034913b1e147e35ada677acaa72d52c3108479b0
Tomoya Iida <silvergirl613@outlook.com>
2023-08-31 04:50:55 +0900

    currency convert complete

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,5 +1,14 @@
-import { useEffect } from "react";
-
+import { useEffect, useState } from "react";
+import currencyConverter from "~/utils/currencyConverter";
 export default function TestPage() {
-  useEffect(() => {}, []);
+  const [data, setData] = useState<any>();
+  useEffect(() => {
+    currencyConverter("EUR", "USD", 27).then((res) => {
+      setData(res);
+    });
+  }, []);
+
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
 }
