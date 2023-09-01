c4d473e6ced57310e0b5967ff579788c631f3f9d
Tomoya Iida <silvergirl613@outlook.com>
2023-09-01 13:23:10 +0900

    MS support02

--- pages/general/booking/CalendarDemo.tsx
+++ pages/general/booking/CalendarDemo.tsx
@@ -1,67 +1,264 @@
-import React from "react";
-import { Box } from "@mui/material";
-import dynamic from "next/dynamic";
+import React, { useEffect } from "react";
+import { enUS } from "date-fns/locale";
+import { format, parse, startOfWeek, getDay } from "date-fns";
 
-const Calendar = dynamic(() => import("@toast-ui/react-calendar"), {
-  ssr: false,
+import {
+  Calendar as BigCalendar,
+  dateFnsLocalizer,
+  SlotInfo,
+  Views,
+} from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import ScheduleAddModal from "./ScheduleAddModal";
+import { Box } from "@mui/material";
+const locales = {
+  "en-US": enUS,
+};
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
 });
 
-import "@toast-ui/calendar/dist/toastui-calendar.min.css";
+type Event = {
+  id: number;
+  title: string;
+  start: Date;
+  end: Date;
+  allDay?: boolean;
+  resource?: any;
+  desc?: string;
+};
 
-const calendarOptions = {
-  // sort of option properties.
+const init: Event[] = [
+  {
+    id: 0,
+    title: "All Day Event very long title",
+    allDay: true,
+    start: new Date(2023, 7, 0),
+    end: new Date(2023, 7, 1),
+  },
+  {
+    id: 1,
+    title: "Long Event",
+    start: new Date(2023, 7, 7),
+    end: new Date(2023, 7, 10),
+  },
+
+  {
+    id: 2,
+    title: "Dating",
+    start: new Date(2023, 8, 1, 0, 0, 0),
+    end: new Date(2023, 8, 1, 0, 0, 0),
+  },
+
+  {
+    id: 3,
+    title: "Meeting with Girlfriend",
+    start: new Date(2023, 8, 6, 0, 0, 0),
+    end: new Date(2023, 8, 8, 22, 0, 0),
+  },
+
+  {
+    id: 4,
+    title: "Some Event",
+    start: new Date(2023, 7, 29, 0, 0, 0),
+    end: new Date(2023, 7, 29, 1, 0, 0),
+  },
+  {
+    id: 5,
+    title: "Conference",
+    start: new Date(2023, 7, 11),
+    end: new Date(2023, 7, 13),
+    desc: "Big conference for important people",
+  },
+  {
+    id: 6,
+    title: "Meeting",
+    start: new Date(2023, 8, 12, 10, 30, 0, 0),
+    end: new Date(2023, 8, 12, 12, 30, 0, 0),
+    desc: "Pre-meeting meeting, to prepare for the meeting",
+  },
+
+  {
+    id: 7,
+    title: "Today",
+    start: new Date(new Date().setHours(new Date().getHours() - 3)),
+    end: new Date(new Date().setHours(new Date().getHours() + 3)),
+  },
+];
+
+const styles = {
+  container: {
+    height: "60vh",
+    margin: "2em",
+  },
 };
 
 export default function CalendarDemo() {
-  const calendars = [{ id: "cal1", name: "Personal" }];
-  const initialEvents = [
-    {
-      id: "1",
-      calendarId: "cal1",
-      title: "Lunch",
-      category: "time",
-      start: "2023-08-28T12:00:00",
-      end: "2023-08-28T13:30:00",
-    },
-    {
-      id: "2",
-      calendarId: "cal1",
-      title: "Coffee Break",
-      category: "time",
-      start: "2023-09-01T15:00:00",
-      end: "2023-09-01T15:30:00",
-    },
-  ];
+  const [events, setEvents] = React.useState(init);
+  const [uid, setUid] = React.useState<number>(10);
+  const [open, setOpen] = React.useState(false);
+  const [title, setTitle] = React.useState<string>("");
+  const [current, setCurrent] = React.useState<any>({});
 
-  // const onAfterRenderEvent = (event) => {
-  //   console.log(event.title);
-  // };
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const setTitleName = (val: string) => setTitle(val);
 
-  const calendarRef = React.useRef<any>();
-
-  const handleClickNextButton = () => {
-    console.log(calendarRef.current.getInstance());
-    // const calendarInstance = calendarRef.current.getInstance();
-    // calendarInstance.next();
+  const ulid = () => {
+    setUid(uid + 1);
+    return uid;
   };
 
+  const handleSelectSlot = ({ start, end }: SlotInfo) => {
+    if (typeof start === "string") {
+      start = new Date(start);
+    }
+
+    if (typeof end === "string") {
+      end = new Date(end);
+    }
+
+    console.log(events, start, end);
+
+    for (let i = 0; i < events.length; i++) {
+      const tempSt = events[i].start;
+      const tempEn = events[i].end;
+      if (end > tempSt && start < tempEn) {
+        alert("You can't schedule with that time range as it is taken.");
+        return;
+      }
+    }
+    handleOpen();
+    setCurrent({ st: start, en: end });
+
+    setEvents([
+      ...events,
+      {
+        id: ulid(),
+        start: new Date(start), //new Date(start.getDay(), start.getHours(), start.getMinutes()),
+        end: new Date(end), //new Date(end.getDay(), end.getHours(), end.getMinutes()),
+        title: "",
+      },
+    ]);
+  };
+
+  const handleSelect = (event: Event) => {
+    const { start, end } = event;
+    const del = window.confirm(
+      `Delete availability on ${format(start, "EEEE")} from ${format(
+        start,
+        "h:mma"
+      )} to ${format(end, "h:mm")}?`
+    );
+    if (del) {
+      const index = events.findIndex((e) => e.id == event.id);
+      setEvents([...events.slice(0, index), ...events.slice(index + 1)]);
+    }
+  };
+
+  useEffect(() => {
+    setEvents((prevEvents) => {
+      if (prevEvents.length === 0) return [];
+      const updatedEvents = [...prevEvents];
+      title !== "none"
+        ? (updatedEvents[updatedEvents.length - 1].title = title)
+        : updatedEvents.pop();
+      return updatedEvents;
+    });
+  }, [title]);
+
+  useEffect(() => {
+    open && setTitle("");
+  }, [open]);
+
   return (
-    <>
-      <Box id="calendar" sx={{ height: "800px" }}>
-        <Calendar
-          view="week"
-          week={{}}
-          month={{
-            dayNames: ["S", "M", "T", "W", "T", "F", "S"],
-            visibleWeeksCount: 5,
-          }}
-          events={initialEvents}
-          ref={calendarRef}
-          {...calendarOptions}
-          // onAfterRenderEvent={onAfterRenderEvent}
-        />
-        <button onClick={handleClickNextButton}>Go next!</button>
-      </Box>
-    </>
+    <Box
+      style={styles.container}
+      sx={{
+        "& .rbc-events-container": {
+          marginRight: "0px !important",
+        },
+        "& .rbc-event": {
+          backgroundColor: "#32b189 !important",
+          border: "1px solid #36c499 !important",
+          borderRadius: "0px !Important",
+        },
+        "& .rbc-allday-cell": {
+          display: "none !important",
+        },
+        "& .rbc-header": {
+          height: "50px",
+          justifyContent: "center",
+          display: "flex",
+          "& span": {
+            color: "#125a45 !important",
+            display: "flex",
+            alignItems: "center",
+          },
+        },
+        "& .rbc-time-content": {
+          "& .rbc-time-column:nth-child(2),.rbc-time-column:nth-child(8)": {
+            background: "#fbf1f1 !important",
+          },
+        },
+        "& .rbc-btn-group": {
+          mb: 1,
+          "& button": {
+            backgroundColor: "#059669 !important",
+            color: "white !important",
+            border: "0px !important",
+            "&:hover": {
+              backgroundColor: "#139e72 !important",
+            },
+          },
+          "& .rbc-active": {
+            backgroundColor: "#23bb8b !important",
+            "&:hover": {
+              backgroundColor: "#23bb8b !important",
+            },
+          },
+        },
+        "& .rbc-agenda-table": {
+          "& .rbc-header": {
+            display: "table-cell !Important",
+            textAlign: "center",
+            color: "#125a45",
+          },
+        },
+      }}
+    >
+      <BigCalendar
+        selectable
+        localizer={localizer}
+        events={events}
+        defaultView={Views.WEEK}
+        views={[
+          Views.DAY,
+          Views.WEEK,
+          Views.MONTH,
+          Views.AGENDA,
+          Views.WORK_WEEK,
+        ]}
+        defaultDate={new Date()}
+        onSelectSlot={handleSelectSlot}
+        onSelectEvent={handleSelect}
+        style={{
+          height: "100%",
+        }}
+        timeslots={2} // number of per section
+      />
+      <ScheduleAddModal
+        open={open}
+        handleOpen={handleOpen}
+        handleClose={handleClose}
+        title={title}
+        setTitle={setTitleName}
+      />
+    </Box>
   );
 }
